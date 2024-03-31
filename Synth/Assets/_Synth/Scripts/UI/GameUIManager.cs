a0fa6e9e16a1955f8fcca652ba5efe5b94ec2d32
Adam <Fireworksanimations21@gmail.com>
2024-03-31 18:26:59 +0200

    Timer voor recording

--- Synth/Assets/_Synth/Scripts/UI/GameUIManager.cs
+++ Synth/Assets/_Synth/Scripts/UI/GameUIManager.cs
@@ -9,11 +9,12 @@ public class GameUIManager : MonoBehaviour
 {
     public GameObject settingsPanel; // Verwijzing naar je Settings Panel
     public Button SettingsButton, CloseButton, RecordButtonStart, RecordButtonStop;
-    public bool Isrecording = false;
+    private bool Isrecording = false, IsCoroutineRunning = false;
     private AudioRecorder audioRecorder;
-    public Color defaultColor; // Standaardkleur
-    public Color toggledColor = Color.red; // Kleur wanneer getoggled
+    private Color defaultColor; // Standaardkleur
+    private Color toggledColor = Color.red; // Kleur wanneer getoggled
     public Dropdown RecordingLengthDropDown;
+    public Text TimerText;
 
     private void Start()
     {
@@ -49,6 +50,7 @@ public class GameUIManager : MonoBehaviour
         audioRecorder.StartRecording();
         Isrecording = true;
         ToggleButtonColor();
+        StartCoroutine(CountdownCoroutine(audioRecorder.RecordingLenghtInt));
     }
 
     public void StopRecording()
@@ -56,10 +58,37 @@ public class GameUIManager : MonoBehaviour
         audioRecorder.StopRecording();
         Isrecording = false;
         ToggleButtonColor();
+        if (IsCoroutineRunning)
+        {
+            StopCoroutine(CountdownCoroutine(audioRecorder.RecordingLenghtInt));
+            TimerText.GetComponent<Transform>().gameObject.SetActive(false);
+        }
     }
 
     public void ToggleButtonColor()
     {
         RecordButtonStart.GetComponent<Image>().color = Isrecording ? toggledColor : defaultColor; // Pas de kleur aan
+    }
+
+    IEnumerator CountdownCoroutine(float duration)
+    {
+        IsCoroutineRunning = true;
+        TimerText.GetComponent<Transform>().gameObject.SetActive(true);
+        float remainingTime = duration;
+        while (remainingTime > 0)
+        {
+            // Update de tekst van het Text object elke frame
+            TimerText.text = "Recording Time: " + Mathf.Ceil(remainingTime).ToString();
+
+            yield return null;
+
+            remainingTime -= Time.deltaTime;
+        }
+
+        // Zet de timer op 0 als de aftelling klaar is
+        TimerText.text = "Recording Time: 0";
+        TimerText.GetComponent<Transform>().gameObject.SetActive(false);
+        IsCoroutineRunning = false;
+        StopRecording(); //stop automatisch de recording als de timer klaar is
     }
 }
